a950c7dcd710d25f0cd591b9c130eeace1fa01e0
Druva Kumar S <druva360@hotmail.com>
2025-12-14 20:10:26 +0530

    custom functions in profit calc

--- 3_profit_calculator/profit_calculator.go
+++ 3_profit_calculator/profit_calculator.go
@@ -3,22 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+	// var revenue float64
+	// var expenses float64
+	// var taxRate float64
 
-	print("Enter Total Revenue:")
-	fmt.Scan(&revenue)
-	print("Enter Total Expenses:")
-	fmt.Scan(&expenses)
-	print("Enter Tax Rate (in %):")
-	fmt.Scan(&taxRate)
+	revenue := getUserInput("Enter Total Revenue: ")
+	expenses := getUserInput("Enter Total Expenses: ")
+	taxRate := getUserInput("Enter Tax Rate (in %): ")
 
-	ebt := revenue - expenses
-	profit := ebt * (1 - taxRate/100)
-	ratio := ebt / profit
+	ebt, profit, ratio := calculateProfits(revenue, expenses, taxRate)
 
 	fmt.Println("Earnings Before Tax (EBT):", ebt)
 	fmt.Println("Net Profit:", profit)
 	fmt.Println("EBT to Profit Ratio:", ratio)
+}
+
+func getUserInput(text string) float64 {
+	var input float64
+	fmt.Print(text)
+	fmt.Scan(&input)
+	return input
+}
+
+func calculateProfits(revenue float64, expenses float64, taxRate float64) (ebt float64, profit float64, ratio float64) {
+	ebt = revenue - expenses
+	profit = ebt * (1 - taxRate/100)
+	ratio = ebt / profit
+	return
 }
